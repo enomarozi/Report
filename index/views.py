bca2e32f15564a397b1eba695e52b2335e4426c9
Eno Marozi <marozieno0@gmail.com>
2025-01-15 15:01:13 +0700

    15-01-2025

--- index/views.py
+++ index/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-	context = {
-		"title":"Dashboard",
-	}
-	return render(request, 'index/index.html', context)
+	return render(request, 'index.html')
+def index1(request):
+	# context = {
+	# 	"title":"Dashboard",
+	# }
+	# return render(request, 'index/index.html', context)
+	return HttpResponse("<h1>ENO MAROZI</h1>")
