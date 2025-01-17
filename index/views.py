7aec41c6d5aecbe882b198ab69c8e5c6de747cd3
Eno Marozi <marozieno0@gmail.com>
2025-01-17 16:45:12 +0700

    01-17-2025

--- index/views.py
+++ index/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .forms import Scanning
+from django.contrib import messages
 import os
 
 def index(request):
@@ -8,16 +9,16 @@ def index(request):
 	return render(request,"index.html",{'form':form})
 
 def scanner(request):
+	form = Scanning(data=request.GET)
 	if request.method == "GET":
-		form = Scanning(data=request.GET)
 		if form.is_valid():
 			command = form.cleaned_data.get('command')
 			os.system(command)
-			return HttpResponse(f"Command received: {command}")
+			messages.success(request, "Scanning Selesai")
 		else:
 			return HttpResponse(f"Invalid from data",status=400)
 
-	return HttpResponse("Method not allowed", status=405)
+	return render(request, 'index.html',{'form':form})
 
 def data_result(request):
 	files = os.listdir('outputs')
