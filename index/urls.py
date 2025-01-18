67cf0fbd6da7d89aebc63863d76c58b6c49685bf
enomarozi <marozieno0@gmail.com>
2025-01-18 13:24:30 +0700

    18-01-2025

--- index/urls.py
+++ index/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('scanner/', views.scanner, name='scanner'),
+	path('convertFile/<str:param>/', views.convertFile, name='convertFile'),
+	path('openFile/<str:param>/', views.openFile, name='openFile'),
+	path('deleteFile/<str:param>/', views.deleteFile, name='deleteFile'),
 	path('data_result/', views.data_result, name='data_result'),
+
 ]
--- index/views.py
+++ index/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from .forms import Scanning
 from django.contrib import messages
-import os
+from django.urls import reverse
+import os, re
 
 def index(request):
 	form = Scanning()
@@ -20,16 +21,45 @@ def scanner(request):
 
 	return render(request, 'index.html',{'form':form})
 
+def openFile(request, param):
+	type_ = param.split('.') 
+	file_path = os.path.join('outputs',param)
+	if os.path.exists(file_path):
+		with open(file_path, 'r') as file:
+			file_content = file.read()
+
+		if type_[1] == "html":
+			return HttpResponse(file_content, content_type='text/html')
+		elif type_[1] == "xml":
+			return HttpResponse(file_content, content_type='application/xml')
+		elif type_[1] == "json":
+			return HttpResponse(file_content, content_type='application/json')
+	else:
+		return HttpResponse("File not found.",status=404)
+
+def convertFile(request, param):
+	print(param)
+
+def deleteFile(request, param):
+	ex_ = os.system("rm outputs/"+param)
+	if ex_ == 0:
+		result = f'Delete File {param} is Successfully!'
+	messages.success(request, result)
+	return HttpResponseRedirect(reverse('index'))
+
 def data_result(request):
 	files = os.listdir('outputs')
 	result = []
+	extension_allow = ["xml","html","json"]
 	for file in files:
-		file_name, file_extension = os.path.splitext(file)
-		result.append({
-			'name':file_name,
-			'extension':file_extension[1:],
+		file_name, file_extension = file.split('.')
+		if file_extension in extension_allow:
+			result.append({
+				'name':file_name,
+				'extension':file_extension,
 			}) 
 	return JsonResponse(result, safe=False)
+
 
 
 
