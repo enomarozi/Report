046807fdbcd30db2909fa9962f6f016abad40de0
Eno Marozi <marozieno0@gmail.com>
2025-02-25 15:50:27 +0700

    25-02-2025

--- index/views.py
+++ index/views.py
@@ -22,8 +22,7 @@ def scanner(request):
 			if validation(command) == True:
 				os.system(command)
 				nama = command.split("outputs/")[1].replace(' ','')
-				saveFiles = OutputFiles(name=nama,date=now(),md5check=checksum_file(nama))
-				saveFiles.save()
+				checksum_file(nama)
 				message = "Scanning Selesai"
 			else:
 				message = "Maaf, Command tidak dizinkan!!"
@@ -68,45 +67,32 @@ def analisaFile(request, param):
 	with open(file_path,'r') as file:
 		data = json.load(file)
 	dict_ = {}
-	if "hosthint" in data["nmaprun"]:
-		if len(data['nmaprun']['hosthint']) <= 3:
-			address = data['nmaprun']['host']['address']['@addr']
-			result = ', '.join([data['nmaprun']['host']['ports']['port'][i]['@portid'] for i in range(len(data['nmaprun']['host']['ports']['port']))])
-			dict_[address] = result
-
+	if "hosthint" not in data["nmaprun"]:
+		dict_["Command"] = data["nmaprun"]["@args"]
+		if "@errormsg" in data['nmaprun']['runstats']['finished']:
+			dict_[data['nmaprun']['runstats']['finished']['@exit']] = data['nmaprun']['runstats']['finished']['@errormsg']
 		else:
-			for i in data['nmaprun']['host']:
-				address = i['address']['@addr']
-				if "port" in i["ports"]:
-					if type(i['ports']['port']) == dict:
-						result = i['ports']['port']['@portid']
-					elif type(i['ports']['port']) == list:
-						result = ', '.join([i['ports']['port'][j]['@portid'] for j in range(len(i['ports']['port']))])
-				else:
-					result = "Down / No Port Active"
-				dict_[address] = result
-	if len(data['nmaprun']['hosthint']) <= 3:
-		address = data['nmaprun']['host']['address']['@addr']
-		result = ', '.join([data['nmaprun']['host']['ports']['port'][i]['@portid'] for i in range(len(data['nmaprun']['host']['ports']['port']))])
-		dict_[address] = result
-
+			dict_[data['nmaprun']['runstats']['finished']['@exit']] = data['nmaprun']['runstats']['finished']['@summary']
 	else:
 		result = {}
-		for i in data['nmaprun']['host']:
-			address = i['address']['@addr']
-			if type(i['ports']['extraports']) == dict:
-				if int(i['ports']['extraports']['@count']) <= 1000:
-					result[i['ports']['extraports']['@state']] = i['ports']['extraports']['@count']
-					if "port" in i['ports']:
-						result["Open"] = ', '.join([i['ports']['port'][j]['@portid'] for j in range(len(i['ports']['port']))])
-			elif type(i['ports']['extraports']) == list:
-				open_ = i['ports']['port']['@portid']
-				for j in range(len(i['ports']['extraports'])):
-					result[i['ports']['extraports'][j]['@state']] = i['ports']['extraports'][j]['@count']
-				
-				result["Open"] = open_
-			dict_[address] = result
-			result = {}
+		if type(data['nmaprun']['host']) == list:
+			for i in data['nmaprun']['host']:
+				address = i['address']['@addr']
+				if type(i['ports']['extraports']) == dict:
+					if int(i['ports']['extraports']['@count']) <= 1000:
+						result[i['ports']['extraports']['@state']] = i['ports']['extraports']['@count']
+						if "port" in i['ports']:
+							result["Open"] = ', '.join([i['ports']['port'][j]['@portid'] for j in range(len(i['ports']['port']))])
+				elif type(i['ports']['extraports']) == list:
+					open_ = i['ports']['port']['@portid']
+					for j in range(len(i['ports']['extraports'])):
+						result[i['ports']['extraports'][j]['@state']] = i['ports']['extraports'][j]['@count']
+					
+					result["Open"] = open_
+				dict_[address] = result
+				result = {}
+		elif type(data['nmaprun']['host']) == dict:
+			print(data['nmaprun']['host']['ports']['port'])
 	return render(request, 'analisa.html',{'analisa':dict_}) 
 
 def deleteFile(request, param):
@@ -127,24 +113,6 @@ def validation(command):
 				return False
 	else:
 		return False
-
-def syncron(request):
-	for i in os.listdir("outputs"):
-		checksum_file(i)
-	message = "Syncronfile Selesai"
-	messages.success(request, message)
-	return redirect('index')
-
-def checksum_file(file):
-	md5hash = hashlib.md5()
-	with open("outputs/"+file,"rb") as f:
-		for chunk in iter(lambda: f.read(4096), b""):
-			md5hash.update(chunk)
-	if OutputFiles.objects.filter(md5check=md5hash.hexdigest()).exists():
-		pass
-	else:
-		saveFiles = OutputFiles(name=file,date=now(),md5check=md5hash.hexdigest())
-		saveFiles.save()
 
 def data_result(request):
 	files = os.listdir('outputs')
--- index/views_convert.py
+++ index/views_convert.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.timezone import now
+from django.contrib import messages
 from .models import OutputFiles
 from lxml import etree
 import xmltodict
 import json
+import os
+import hashlib
 
 def xml_to_html(file_path, xslt_file):
 	xml_tree = etree.parse(file_path)
@@ -13,10 +16,9 @@ def xml_to_html(file_path, xslt_file):
 
 	filename = file_path.replace("xml","html")
 	nama = filename.split("outputs/")[1].replace(' ','')
-	saveFiles = OutputFiles(name=nama,date=now())
-	saveFiles.save()
 	with open(filename, "wb") as f:
 		f.write(etree.tostring(html_tree, pretty_print=True))
+	checksum_file(nama)
 
 def xml_to_json(file_path):
 	with open(file_path,"r") as xml_file:
@@ -26,7 +28,24 @@ def xml_to_json(file_path):
 	json_string = json.dumps(xml_dict, indent=4)
 	filename = file_path.replace("xml","json")
 	nama = filename.split("outputs/")[1].replace(' ','')
-	saveFiles = OutputFiles(name=nama,date=now())
-	saveFiles.save()
 	with open(filename, "w") as f:
 		f.write(json_string)
+	checksum_file(nama)
+
+def syncron(request):
+	for i in os.listdir("outputs"):
+		checksum_file(i)
+	message = "Syncronfile Selesai"
+	messages.success(request, message)
+	return redirect('index')
+
+def checksum_file(file):
+	md5hash = hashlib.md5()
+	with open("outputs/"+file,"rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			md5hash.update(chunk)
+	if OutputFiles.objects.filter(md5check=md5hash.hexdigest()).exists():
+		pass
+	else:
+		saveFiles = OutputFiles(name=file,date=now(),md5check=md5hash.hexdigest())
+		saveFiles.save()
